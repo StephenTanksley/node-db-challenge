17a7e678084442f43acbe29bf4a1912fb5145db5
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-01-11 12:29:40 -0600

    Added some tasks to the seed database.

--- data/seeds/03-tasks.js
+++ data/seeds/03-tasks.js
@@ -1,11 +1,11 @@
-
 exports.seed = function(knex) {
   return knex('tasks').del()
     .then(function () {
       return knex('tasks').insert([
-        {id: 1, description: 'rowValue1', notes: "", completed: false},
-        {id: 2, description: 'rowValue2', notes: "", completed: false},
-        {id: 2, description: 'rowValue2', notes: "", completed: false},      
+        {id: 1, description: 'Log on to github.', notes: "Click on the clickety click.com", completed: false, project_id: 1},
+        {id: 2, description: 'Take sprint exam', notes: "Code for your liiiiiife", completed: false, project_id: 1},
+        {id: 3, description: 'Vacuum', notes: "vacuum up the dirt in the great room. The puppies have a tendency of bringing in dirt from the yard.", completed: false, project_id: 2},      
+        {id: 4, description: 'Scrub sink', notes: "Use the sponges from under the sink and the cleaner to get it looking nice. Mom always hates a dirty sink.", completed: false, project_id: 2}, 
       ]);
     });
 };
